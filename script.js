025902f224cba16ecbc3b758d2838b5ab318cddc
Veronika <72261304+knika-starkey@users.noreply.github.com>
2021-09-17 22:36:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ for(let i = 1; i < 64; i++){
     seed *= 2;
     sum += seed;
 }
-let weight = sum*0.065;
+let weight = sum*0.065/1000;
 alert(sum + " зерен составляла награда и " + weight + " килограмм зерна мудрец должен был получить");
